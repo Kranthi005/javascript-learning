3620a6dfe5b35e9385d4b823ace4d78a2e686077
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-12 22:48:15 +0530

    Implemented the main game logic iside the the pre defined click

--- mini-projects/guessNumber/script.js
+++ mini-projects/guessNumber/script.js
@@ -5,10 +5,34 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-const displayNumber = function (message) {
+const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
 
 document.querySelector(".check").addEventListener("click", function () {
   const guessNumber = document.querySelector(".guess").value;
+
+  if (!guessNumber) {
+    displayMessage("⛔️ No number!");
+  } else if (guessNumber == secretNumber) {
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    if (score > highScore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guessNumber != secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guessNumber > secretNumber ? "📈 Too high!" : "📉 Too low!",
+      );
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
 });
+
