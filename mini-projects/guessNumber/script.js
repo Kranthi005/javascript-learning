4a69359aac30a131afc65b837cd02a97839a13b2
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-12 22:33:09 +0530

    Initalized variables and a funtion shows the message

--- mini-projects/guessNumber/script.js
+++ mini-projects/guessNumber/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// This generates a random number from 1 to 20
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayNumber = function (message) {
+  document.querySelector(".message").textContent = message;
+};
