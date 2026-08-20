ad2025fc1f0d813006dc1ad336e37a94a05b673b
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-20 13:31:03 +0530

    Repete the function of the button click event to the rest of buttons

--- mini-projects/colorPicker/script.js
+++ mini-projects/colorPicker/script.js
@@ -5,3 +5,18 @@ function changeBgToGreyAndUpdateText() {
   bgColor.style.backgroundColor = "#e0e0e0";
   colorName.textContent = "#e0e0e0";
 }
+
+function changeBgToGreenAndUpdateText() {
+  bgColor.style.backgroundColor = "#6fcf97";
+  colorName.textContent = "#6fcf97";
+}
+
+function changeBgToBlueAndUpdateText() {
+  bgColor.style.backgroundColor = "#56ccf2";
+  colorName.textContent = "#56ccf2";
+}
+
+function changeBgToPurpleAndUpdateText() {
+  bgColor.style.backgroundColor = "#bb6bd9";
+  colorName.textContent = "#bb6bd9";
+}
