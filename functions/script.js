0579f141d12e4232dbdd378f96d3080ad200dfbb
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-25 21:34:16 +0530

    Practice funtion declaration and expression

---
+++ functions/script.js
@@ -0,0 +1,13 @@
+//Function Declaration
+function showMessage() {
+  console.log("Hello");
+}
+
+showMessage();
+
+//Function Expression
+let showMessage = function () {
+  console.log("Hello");
+};
+
+showMessage();
