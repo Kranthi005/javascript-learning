4b099e951fde2cb11963c90113755ac5a7d290ae
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-25 20:29:42 +0530

    Used the querySelectorAll this make node list used in the modelWindow project

---
+++ mini-projects/modelWindow/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const model = document.querySelector(".model");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-model");
+
+//The querySelector only selects the first of the many elements with same class name
+//So we use querySelectorAll to make them as arrya
+
+const btnOpenModel = document.querySelectorAll(".show-model");
