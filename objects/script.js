9f8b88fb6bb7ed2c032d643e52c1243b489f095a
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-25 21:55:48 +0530

    More practice on objects on the concepts in js

--- objects/script.js
+++ objects/script.js
@@ -22,3 +22,58 @@ console.log(person[a]); //Kumar
 let { firstName, age } = person;
 console.log(firstName);
 console.log(age);
+
+//Modifying Object Property
+//Dot Notation
+person.firstName = "Abhi";
+console.log(person.firstName);
+//Bracket Notation
+person["firstName"] = "Abhi";
+console.log(person["firstName"]);
+
+//Adding Object Property
+//Dot Notation
+person.gender = "Male";
+console.log(person);
+//Brackt Notation
+person["gender"] = "Male";
+console.log(person);
+
+//Property Value
+
+//Function of value
+let person = {
+  firstName: "Rahul",
+  lastName: "Attuluri",
+  age: 20,
+  run: function () {
+    console.log("Start Running.");
+  },
+};
+person.run();
+
+//Array as Value
+let person = {
+  firstName: "Rahul",
+  lastName: "Attuluri",
+  age: 28,
+  habits: ["Playing Chess", "Singing"],
+};
+console.log(person.habits);
+console.log(person.habits[0]);
+console.log(person["habits"][1]);
+
+//Object as a Value
+let person = {
+  firstName: "Rahul",
+  lastName: "Attuluri",
+  age: 28,
+  habits: ["Playing Chess", "Singing", "Dancing"],
+  car: {
+    name: "Audi",
+    model: "A6",
+    color: "White",
+  },
+};
+console.log(person.car.name);
+console.log(person.car["model"]);
