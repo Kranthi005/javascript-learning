434262d2627aaaa2874c0b23f7949b969203324a
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-25 21:31:24 +0530

    Practice the Array concept in js

---
+++ array/script.js
@@ -0,0 +1,27 @@
+//Creating an Array
+let myArray = [5, "six", 2, 2.2];
+console.log(myArray);
+
+//Accessing an Array Item
+console.log(myArray[0]);
+console.log(myArray[1]);
+
+//Modifying an Array Item
+myArray[1] = 6;
+console.log(myArray);
+
+//Finding Array Length
+let lengthOfArray = myArray.length;
+console.log(lengthOfArray);
+
+//Array Methods
+
+//push
+myArray.push(true);
+console.log(myArray);
+
+//pop
+let lastIteam = myArray.pop();
+console.log(lastIteam);
+
+
