75c65ddb48204aedea3ee0641971d8df5d4e1456
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-25 20:39:30 +0530

    Make the opening event on click on the button in modelWindow miniproject

--- mini-projects/modelWindow/script.js
+++ mini-projects/modelWindow/script.js
@@ -1,10 +1,19 @@
 "use strict";
 
-const model = document.querySelector(".model");
+const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModel = document.querySelector(".close-model");
+const btnCloseModel = document.querySelector(".close-modal");
 
 //The querySelector only selects the first of the many elements with same class name
 //So we use querySelectorAll to make them as arrya
 
-const btnOpenModel = document.querySelectorAll(".show-model");
+const btnOpenModel = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  //Adding a event listener
+  btnOpenModel[i].addEventListener("click", function () {
+    //The class should pass with out the .
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
