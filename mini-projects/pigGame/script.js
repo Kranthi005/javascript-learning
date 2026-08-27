0a989147fa4b96c4516911494605770e68ffdff0
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-27 20:38:45 +0530

    In pig game initilized veriables and hide the dice and set the player score to 0

---
+++ mini-projects/pigGame/script.js
@@ -0,0 +1,7 @@
+let playerOneScore = document.querySelector("#score--0");
+let playerTwoScore = document.querySelector("#score--1");
+let diceImage = document.querySelector(".dice");
+
+playerOneScore.textContent = "0";
+playerTwoScore.textContent = "0";
+diceImage.classList.add("hide");
