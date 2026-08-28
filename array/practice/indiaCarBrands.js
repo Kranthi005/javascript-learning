f7e5b8d56f8b87156fe3387d9b6baa1136477b2a
Kranthi005 <kranthikumar11422@gmail.com>
2026-08-28 18:39:22 +0530

    Practice about the array and object in js with questions

---
+++ array/practice/indiaCarBrands.js
@@ -0,0 +1,36 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // arrayOfCarBrands
+  let arrayOfCarBrands = JSON.parse(readLine().replace(/'/g, '"'));
+  let ans = [];
+  for (let i of arrayOfCarBrands) {
+    if (i.country == "India") {
+      ans.push(i.name);
+    }
+  }
+  console.log(ans);
+}
